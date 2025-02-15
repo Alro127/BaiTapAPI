32c2bd00b46327c2bd946d5abdceb88c1bb33093
Alro127 <nhuttan373@gmail.com>
2025-02-15 19:58:45 +0700

    first commit

---
+++ src/main/java/it/tanque/ltdd/LtddApplication.java
@@ -0,0 +1,13 @@
+package it.tanque.ltdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LtddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LtddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/tanque/ltdd/LtddApplicationTests.java
@@ -0,0 +1,13 @@
+package it.tanque.ltdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LtddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
